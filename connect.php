66c4d560a255a9dd8cd5f3c2b4e866d4c47b2816
swapnilpawar974 <87270427+swapnilpawar974@users.noreply.github.com>
2024-06-06 10:48:52 +0000

    changes to database connection

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = tcp:saheli-digital-studio.database.windows.net,1433; Database = saheliData", "azure", "Saheli@111");
+        $conn = new PDO("sqlsrv:server = tcp:saheli-digital-studio.database.windows.net,1433; Database = sphotodata", "azure", "Saheli@111");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         print("Error connecting to SQL Server.");
@@ -9,7 +9,7 @@
     }
 
     // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "azure", "pwd" => "Saheli@111", "Database" => "saheliData", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $connectionInfo = array("UID" => "azure", "pwd" => "Saheli@111", "Database" => "sphotodata", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:saheli-digital-studio.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
 
